82699bc5323c4f110c432cc05eb3d4e728fb952c
Cristian Rodriguez <43813175+alu0101020730@users.noreply.github.com>
2020-12-23 15:07:21 +0000

    Add files via upload

---
+++ Scripts/Listener.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using UnityEngine.UI;
+
+public class Listener : MonoBehaviour
+{
+    [SerializeField] private string[] m_Keywords;
+    [SerializeField] InputField input;
+    private KeywordRecognizer m_Recognizer;
+
+    void Start()
+    {
+        input.onEndEdit.AddListener(KeywordArrayMaker);
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.AppendFormat("{0} ({1}){2}", args.text, args.confidence, Environment.NewLine);
+        builder.AppendFormat("\tTimestamp: {0}{1}", args.phraseStartTime, Environment.NewLine);
+        builder.AppendFormat("\tDuration: {0} seconds{1}", args.phraseDuration.TotalSeconds, Environment.NewLine);
+        Debug.Log(builder.ToString());
+    }
+
+    private void KeywordArrayMaker(string arg)
+    {
+        m_Keywords = arg.Split(',');
+    }
+
+    public void StartKeyword()
+    {
+        m_Recognizer = new KeywordRecognizer(m_Keywords);
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        m_Recognizer.Start();
+    }
+
+    public void StopKeyword()
+    {
+        m_Recognizer.Stop();
+        m_Recognizer.Dispose();
+        PhraseRecognitionSystem.Shutdown();
+    }
+}
